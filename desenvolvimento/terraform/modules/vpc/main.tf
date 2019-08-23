6de3a083c017701454631f8f93df3dfbc352799b
Thsx <50934142+Thsx@users.noreply.github.com>
2019-08-23 09:58:43 -0300

    mudanças no codigo da vpc

--- desenvolvimento/terraform/modules/vpc/main.tf
+++ desenvolvimento/terraform/modules/vpc/main.tf
@@ -1,35 +1,44 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_vpc}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "Devopszando"
-  }
-  }
-
+resource "aws_vpc" "Teste_Vpc" {
+  cidr_block = "${var.cidr_block_vpc}"
+}
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.Teste_Vpc.id}"
  }
 
-resource "aws_subnet" "subnet_public-D" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.cidr_subnet}"
-  map_public_ip_on_launch = "true"
-  availability_zone = "${var.availability_zone}"
-  tags = {
-    Name = "SUBNET | ZONE-D | 192.168.1.0/24"
-  }
-  }
+  
+  resource "aws_subnet" "Public" {
+  vpc_id     = "${aws_vpc.Teste_Vpc.id}"
+  cidr_block = "${var.cidr_block_public}"
 
+  tags = {
+    Name = "Public"
+  }
+}
+resource "aws_subnet" "Private" {
+  vpc_id     = "${aws_vpc.Teste_Vpc.id}"
+  cidr_block = "${var.cidr_block_private}"
+
+  tags = {
+    Name = "Private"
+  }
+}
 resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.Teste_Vpc.id}"
 route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
   }
 }
 
-resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.subnet_public-D.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
+resource "aws_route_table_association" "route" {
+
+    subnet_id = "${aws_subnet.Public.id}"
+    route_table_id = "${aws_route_table.rtb_public.id}"
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.Teste_Vpc.id}"
+
+  tags = {
+    Name = "git-gateway"
+  }
 }
