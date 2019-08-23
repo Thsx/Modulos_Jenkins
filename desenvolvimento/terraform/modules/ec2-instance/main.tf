246e8ef63a6c0e26fe4f9447e539c61752eea1b6
Thsx <50934142+Thsx@users.noreply.github.com>
2019-08-23 09:50:25 -0300

    Fiz alterações no codigo main retirei o backend

--- desenvolvimento/terraform/modules/ec2-instance/main.tf
+++ desenvolvimento/terraform/modules/ec2-instance/main.tf
@@ -1,32 +1,21 @@
 provider "aws" {
-region      = "${var.region}"
-}
-terraform {
-  backend "s3" {
-    bucket = "terraform-thi"
-    key    = "instancia2/ec2/terraform.tfstate"
-    region = "us-west-1"
-  }
+region = "${var.region}"
 }
 resource "aws_instance" "Teste" {
   ami = "${var.ami}"
   instance_type = "${var.type}"
+  ##security_groups = ["${aws_security_group.security.id}"]
+  associate_public_ip_address = "true"
     
-  user_data = <<-EOF
-   #!/bin/bash
-   echo "Hello, World" > index.html
-   nohup busybox httpd -f -p "${var.port}" &
-   EOF
-
   tags {
-    Name = "Geladeira_Tsunami"
+    Name = "ci/cd gitlab"
   }
   }
   resource "aws_vpc" "Teste_Vpc" {
   cidr_block = "${var.cidr_block_vpc}"
 }
 
-resource "aws_security_group" "seg" {
+resource "aws_security_group" "security" {
 vpc_id = "${aws_vpc.Teste_Vpc.id}"
   ingress {
     from_port   = "${var.port}"
@@ -34,15 +23,22 @@ vpc_id = "${aws_vpc.Teste_Vpc.id}"
     protocol    = "${var.i_protocol}"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port   = "${var.e_port}"
-    to_port     = "${var.e_port}"
+  ingress {
+    from_port   = "${var.port_https}"
+    to_port     = "${var.port_https}"
     protocol    = "${var.i_protocol}"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = "${var.port_sh}"
+    to_port     = "${var.port_sh}"
+    protocol    = "${var.i_protocol}"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
 
   tags {
-    Name = "Paredao"
+    Name = "Security Group Instance"
   }
 }
 resource "aws_internet_gateway" "igw" {
@@ -69,12 +65,20 @@ resource "aws_subnet" "Private" {
 resource "aws_route_table" "rtb_public" {
   vpc_id = "${aws_vpc.Teste_Vpc.id}"
 route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
   }
 }
 
-resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.Public.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
+resource "aws_route_table_association" "route" {
+
+    subnet_id = "${aws_subnet.Public.id}"
+    route_table_id = "${aws_route_table.rtb_public.id}"
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.Teste_Vpc.id}"
+
+  tags = {
+    Name = "git-gateway"
+  }
 }
