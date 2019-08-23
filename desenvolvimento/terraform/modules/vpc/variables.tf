d485cd09d3954b1cc94986d5806852f8d096b7d3
Thsx <50934142+Thsx@users.noreply.github.com>
2019-08-23 09:59:18 -0300

    mudanças nas variaveis

--- desenvolvimento/terraform/modules/vpc/variables.tf
+++ desenvolvimento/terraform/modules/vpc/variables.tf
@@ -1,13 +1,9 @@
-variable "cidr_vpc" {
-  description = "CIDR Bloco da VPC"
-  default = "192.168.0.0/16"
+variable "cidr_block_public" {
+  default = "10.0.0.0/24"
 }
-variable "cidr_subnet" {
-  description = "Subnet Zona D 1.0/24"
-  default = "192.168.1.0/24"
+variable "cidr_block_private" {
+  default = "10.0.1.0/24"
 }
-
-variable "availability_zone" {
-  description = "Zona de Disponibilidade da SubNet"
-  default = "us-west-1a"
+variable "cidr_block_vpc"{
+    default = "10.0.0.0/16"
 }
