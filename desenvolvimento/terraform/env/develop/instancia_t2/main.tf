495c31de4c752b248d83d06d2b2a848516f60dcc
Thsx <50934142+Thsx@users.noreply.github.com>
2019-08-27 15:06:51 -0300

    main atualizado e variables

--- desenvolvimento/terraform/env/develop/instancia_t2/main.tf
+++ desenvolvimento/terraform/env/develop/instancia_t2/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
 }
 
 module "ec2-instance" {
     source = "../../../../../desenvolvimento/terraform/modules/ec2-instance/"
-    
+    vpc_id = "${module.vpc.vpc_id}"
+}
+module "vpc" {
+    source = "../../../../../desenvolvimento/terraform/modules/vpc/"
+    #vpc_id = "${module.vpc.vpc_id}"
 }
